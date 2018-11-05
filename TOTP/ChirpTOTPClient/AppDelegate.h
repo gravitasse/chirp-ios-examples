40f672aecfb53e3590a09db2e43459236cbee2ad
Joe Todd <joe@chirp.io>
2018-11-05 13:39:28 +0000

    Add TOTP example

---
+++ TOTP/ChirpTOTPClient/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  chirpTOTP
+//
+//  Created by Amogh Matt on 08/06/2018.
+//  Copyright © 2018 Chirp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
